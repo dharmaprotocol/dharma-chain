0df00c5b016fae0e2bca7850cabbe0753edb39fe
Chris Min <chris@dharma.io>
2018-07-12 13:18:17 -0700

    fix: run block mining in separate process

--- index.js
+++ index.js
@@ -6,55 +6,54 @@ const requiredGanache = "ganache-cli@6.1.3";
 
 const scriptsDir = `${__dirname}/scripts`;
 
-console.log('Checking Ganache Version...');
+console.log("Checking Ganache Version...");
 
-console.log(
-    "Running:", `npm list ${requiredGanache} -g || npm install ${requiredGanache} -g`
-);
+console.log("Running:", `npm list ${requiredGanache} -g || npm install ${requiredGanache} -g`);
 
 const installGanacheProcess = child_process.exec(
     `npm list ${requiredGanache} -g || npm install ${requiredGanache} -g`,
     (error, stdout, stderr) => {
         if (stdout) {
-            console.log('Found ganache-cli:', stdout);
+            console.log("Found ganache-cli:", stdout);
         }
 
         if (stderr) {
-            console.log('stderr:', stderr);
+            console.log("stderr:", stderr);
         }
 
         if (error !== null) {
             console.log(`exec error: ${error}`);
         }
-    },
+    }
 );
 
 // Once we have installed Ganache-CLI, we can init a chain and migrate the contracts.
 installGanacheProcess.on("exit", () => {
     const initChainProcess = child_process.spawn(`${scriptsDir}/init_chain.sh`);
+    const mineBlockProcess = child_process.spawn(`${scriptsDir}/mine_block.sh`);
 
     // True once ganache-cli has begun.
     let chainStarted = false;
 
-    initChainProcess.on('exit', function (code, signal) {
+    initChainProcess.on("exit", function(code, signal) {
         if (!chainStarted) {
-            console.error(`
-Local blockchain (running on ganache-cli) failed to start!
-        `);
+            console.error(`Local blockchain (running on ganache-cli) failed to start!`);
         }
 
-        console.error(`
-Local blockchain (ganache-cli) exited with
-code ${code} and signal ${signal}
-    `);
+        console.error(`Local blockchain (ganache-cli) exited with code ${code} and signal ${signal}`);
     });
 
-    initChainProcess.stdout.on('data', function (data) {
+    initChainProcess.stdout.on("data", function(data) {
         chainStarted = true;
         console.log(data.toString());
     });
 
-    process.on('exit', () => {
+    mineBlockProcess.stdout.on("data", function(data) {
+        console.log(data.toString());
+    });
+
+    process.on("exit", () => {
         initChainProcess.kill();
+        mineBlockProcess.kill();
     });
 });
--- scripts/init_chain.sh
+++ scripts/init_chain.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
 BASEDIR=$(dirname "$0")
-
 ganache-cli --db="$BASEDIR/../data" --networkId 70 --accounts 20 --deterministic --mnemonic="arm impose enemy alpha bird attend hunt host town sleep charge catalog"
-curl http://localhost:8545 -X POST --data '{"jsonrpc":"2.0","method":"evm_mine","params":[],"id":1}'
