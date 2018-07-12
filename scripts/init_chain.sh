a1ceed090bf84111a6d9f6f15c0800cd40b1f3f6
Chris Min <chris@dharma.io>
2018-07-11 18:56:34 -0700

    feat: mine a single block when the blockchain starts

--- scripts/init_chain.sh
+++ scripts/init_chain.sh
@@ -3,3 +3,4 @@
 BASEDIR=$(dirname "$0")
 
 ganache-cli --db="$BASEDIR/../data" --networkId 70 --accounts 20 --deterministic --mnemonic="arm impose enemy alpha bird attend hunt host town sleep charge catalog"
+curl http://localhost:8545 -X POST --data '{"jsonrpc":"2.0","method":"evm_mine","params":[],"id":1}'
