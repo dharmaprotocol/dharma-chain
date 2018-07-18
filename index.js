d5e7a7ff35ad9f5112533c4480e8f73e2f029d49
Graeme <graemelearn@protonmail.com>
2018-07-18 13:55:17 -0700

    fix: use dataDir var

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ const child_process = require("child_process");
 const requiredGanache = "ganache-cli@6.1.3";
 
 const scriptsDir = `${__dirname}/scripts`;
+const dataDir = `${__dirname}/data`;
 
 console.log("Checking Ganache Version...");
 
@@ -30,8 +31,8 @@ const installGanacheProcess = child_process.exec(
 // Once we have installed Ganache-CLI, we can init a chain and migrate the contracts.
 installGanacheProcess.on("exit", async () => {
     console.log("Replacing working data with pristine blockchain state");
-    await child_process.exec("rm -r data/working");
-    await child_process.exec("cp -r ./data/pristine ./data/working");
+    await child_process.exec(`rm -r ${dataDir}/working`);
+    await child_process.exec(`cp -r ${dataDir}/pristine ${dataDir}/working`);
 
     const initChainProcess = child_process.spawn(`${scriptsDir}/init_chain.sh`);
     const mineBlockProcess = child_process.spawn(`${scriptsDir}/mine_block.sh`);
